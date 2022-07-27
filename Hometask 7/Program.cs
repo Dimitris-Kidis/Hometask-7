55a6a862dbc2083a6a60fa507e1dca0391918904
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-07-27 23:04:29 +0300

    Utility Class (plus static methods) & Static Constructor Implemented

--- Hometask 7/Program.cs
+++ Hometask 7/Program.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿// Hometask #7
+
+// 1. Class Angle. 1 deg = 60 mins, 1 min = 60 secs. 
+//    Overload + and - operators. ✅
+// 2. Create a class and create a custom sorting using ...
+// 3. Create a utility class ( 3.1. Use static methods) ✅
+// 4. Use static constructor ✅
+
+using System;
+using static Helper;
+using System.IO;
+using System.Text;
 
 
 public class App
 {
     public static void Main(string[] args)
     {
+        //Angle LOG = new Angle();
         Angle a = new Angle(5, 26, 12);
         Angle b = new Angle(3, 7, 34);
         Angle c = new Angle(0, 36, 2);
@@ -26,7 +38,10 @@ public class App
 
         Console.WriteLine(f);
 
-
+        
+        Console.WriteLine(NearestSquareRoot(5));
+        Console.WriteLine(RomanToInteger("XIX"));
+        Console.WriteLine(IntToRoman(21));
 
     }
 }
@@ -37,6 +52,26 @@ public class Angle
     public int min { get; set; }
     public int sec { get; set; }
 
+    static Angle ()
+    {
+        try
+        {
+            // If techcoil.txt exists, seek to the end of the file,
+            // else create a new one.
+            FileStream fileStream = File.Open("hometask7.txt",
+                FileMode.Append, FileAccess.Write);
+            // Encapsulate the filestream object in a StreamWriter instance.
+            StreamWriter fileWriter = new StreamWriter(fileStream);
+            // Write the current date time to the file
+            fileWriter.WriteLine("Compiled at " + System.DateTime.Now.ToString() + " by " + Environment.UserName + " on " + Environment.OSVersion);
+            fileWriter.Flush();
+            fileWriter.Close();
+        }
+        catch (IOException ioe)
+        {
+            Console.WriteLine(ioe);
+        }
+    }
     public Angle(int deg, int min, int sec)
     {
         Console.ForegroundColor = ConsoleColor.Red;
@@ -112,4 +147,63 @@ public class Angle
 
         return new Angle(marker * Math.Abs(g), marker * Math.Abs(m), marker * Math.Abs(s));
     }
+}
+
+
+
+class Helper
+{
+    public static int NearestSquareRoot(int n)
+    {
+        return (int)Math.Pow(Math.Round(Math.Sqrt(n)), 2);
+    }
+
+    private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    public static int RomanToInteger(string roman)
+    {
+        int number = 0;
+        for (int i = 0; i < roman.Length; i++)
+        {
+            if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+            {
+                number -= RomanMap[roman[i]];
+            }
+            else
+            {
+                number += RomanMap[roman[i]];
+            }
+        }
+        return number;
+    }
+
+    public static string IntToRoman(int number)
+    {
+        if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("Insert value betwheen 1 and 3999");
+        if (number < 1) return string.Empty;
+        if (number >= 1000) return "M" + IntToRoman(number - 1000);
+        if (number >= 900) return "CM" + IntToRoman(number - 900);
+        if (number >= 500) return "D" + IntToRoman(number - 500);
+        if (number >= 400) return "CD" + IntToRoman(number - 400);
+        if (number >= 100) return "C" + IntToRoman(number - 100);
+        if (number >= 90) return "XC" + IntToRoman(number - 90);
+        if (number >= 50) return "L" + IntToRoman(number - 50);
+        if (number >= 40) return "XL" + IntToRoman(number - 40);
+        if (number >= 10) return "X" + IntToRoman(number - 10);
+        if (number >= 9) return "IX" + IntToRoman(number - 9);
+        if (number >= 5) return "V" + IntToRoman(number - 5);
+        if (number >= 4) return "IV" + IntToRoman(number - 4);
+        if (number >= 1) return "I" + IntToRoman(number - 1);
+        throw new ArgumentOutOfRangeException("Something bad happened");
+    }
+
 }
